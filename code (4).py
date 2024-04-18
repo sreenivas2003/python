ce778e6127a42288d79cb45a80681c37f5f6f3ab
sreenivas2003 <167345349+sreenivas2003@users.noreply.github.com>
2024-04-18 10:40:24 +0530

    Add files via upload

---
+++ code (4).py
@@ -0,0 +1,24 @@
+word = input()
+
+print(word)
+
+
+store = dict()
+print(store)
+store['P'] = 23
+store['Q'] = 12
+store[11]=13
+store["hello"] = 222
+print(store)
+
+#allkeys = store.keys()
+#print(allkeys)
+
+#for each loop
+#for ele in allkeys:
+# print(ele)
+
+#  for ch in "abcdef":
+#  print(ch)
+
+    
