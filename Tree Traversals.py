f6ef9ea85fc746b9f9b626019b2594c11b3a7410
sreenivas2003 <167345349+sreenivas2003@users.noreply.github.com>
2024-04-28 12:13:01 +0530

    Create Tree Traversals.py

---
+++ Tree Traversals.py
@@ -0,0 +1,66 @@
+class TreeNode:
+    def _init_(self, data):
+        self.data = data 
+        self.left = None 
+        self.right = None 
+ 
+ 
+def printPreOrder(root):
+    if root == None:
+        return
+ 
+    # root, left_subtree, right_subtree 
+    print(root.data, end = " ")
+    printPreOrder(root.left)
+    printPreOrder(root.right)
+ 
+def printInOrder(root):
+    if root == None:
+        return
+ 
+    # left_subtree, root, right_subtree 
+ 
+    printInOrder(root.left)
+    print(root.data, end = " ")
+    printInOrder(root.right) 
+ 
+def printPostOrder(root):
+    if root == None:
+        return
+ 
+    # left_subtree, right_subtree, root
+ 
+    printPostOrder(root.left)
+    printPostOrder(root.right) 
+    print(root.data, end = " ")
+ 
+# 1. objects creation (memory allocation) 
+root = TreeNode(18)
+# root:
+# data = 15 
+# left = None 
+# right = None 
+obj2 = TreeNode(15)
+obj3 = TreeNode(20)
+obj4 = TreeNode(25)
+obj5 = TreeNode(30)
+obj6 = TreeNode(45)
+obj7 = TreeNode(80)
+ 
+#    18 
+#   15  20 
+# 25 30 45 80
+ 
+ 
+# 2- establishing links between nodes 
+root.left = obj2 
+root.right = obj3 
+ 
+obj2.left = obj4 
+obj2.right = obj5 
+ 
+obj3.left = obj6 
+obj3.right = obj7
+printInOrder(root)
+printPreOrder(root)
+printPostOrder(root)
